bc4287b5bd9be5b004c446c3a3b389224d6304ac
ayobami <mrayobamioluwaseyi@gmail.com>
2022-11-16 10:58:29 +0000

    script
    Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,56 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float verticalInput;
+    private const float  LANE_DISTANCE = 5.0f;
+    private CharacterController controller;
+    private float speed = 5.0f;
+    private float verticalInput;
+    private int desiredLane = 1; 
+
 
     // Start is called before the first frame update
-    void Start()
+   private void Start()
     {
-        
+      controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+   private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        MoveLane(false);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        MoveLane(true);
 
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        if (desiredLane == 0) // left
+        {
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        }
+        else if (desiredLane == 2) // right
+        {
+            targetPosition += Vector3.right * LANE_DISTANCE;
+        }
+        // calculate delta move
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+        moveVector.y = -0.1f;
+        moveVector.z = speed;
+
+        //move wakanda
+        controller.Move(moveVector * Time.deltaTime);
+
+        
+      //  verticalInput = Input.GetAxis("Vertical");
+       // transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+    }
+    private void MoveLane (bool goingRight) {
+        //left
+ desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
     }
 }
